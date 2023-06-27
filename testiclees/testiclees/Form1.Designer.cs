f7dd5b1d44f7d6361c8ef08bf2555853f1808f06
Scarlike <137900071+Scarlike@users.noreply.github.com>
2023-06-27 22:53:41 +0800

    S

--- testiclees/testiclees/Form1.Designer.cs
+++ testiclees/testiclees/Form1.Designer.cs
@@ -28,37 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.SuspendLayout();
+            label1 = new Label();
+            button1 = new Button();
+            SuspendLayout();
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe UI", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(311, 169);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(171, 60);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "HIIIIIIII";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 25.8F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(272, 127);
+            label1.Name = "label1";
+            label1.Size = new Size(135, 47);
+            label1.TabIndex = 0;
+            label1.Text = "HIIIIIIII";
+            label1.Click += label1_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(302, 213);
+            button1.Name = "button1";
+            button1.Size = new Size(105, 72);
+            button1.TabIndex = 1;
+            button1.Text = "START";
+            button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Red;
-            this.ClientSize = new System.Drawing.Size(914, 600);
-            this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Red;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Label label1;
+        private Button button1;
     }
 }
